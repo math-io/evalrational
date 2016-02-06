63e51959cfb84906155e57081723e09e40ad33ad
Planeshifter <pburckhardt@outlook.com>
2016-02-06 18:51:11 -0500

    Add example

---
+++ examples/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var round = require( 'math-round' );
+var evalrational = require( './../lib' );
+
+var len;
+var num;
+var denom;
+var sign;
+var rational;
+var v;
+var i;
+
+// Create two arrays of random coefficients...
+len = 10;
+num = new Float64Array( len );
+denom = new Float64Array( len );
+for ( i = 0; i < len; i++ ) {
+	if ( Math.random() < 0.5 ) {
+		sign = -1;
+	} else {
+		sign = 1;
+	}
+	num[ i ] = sign * round( Math.random()*100 );
+	denom[ i ] = sign * round( Math.random()*100 );
+}
+
+// Evaluate the rational function at random values...
+for ( i = 0; i < 100; i++ ) {
+	v = Math.random() * 100;
+	console.log( 'f(%d) = %d', v, evalrational( num, denom, v ) );
+}
+
+// Generate an `evalrational` function...
+rational = evalrational.factory( num, denom );
+for ( i = 0; i < 100; i++ ) {
+	v = Math.random()*100 - 50;
+	console.log( 'f(%d) = %d', v, rational( v ) );
+}
