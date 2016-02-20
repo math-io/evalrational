ef395af7dcbecd2de340405ff9f032680e34950c
kgryte <kgryte@gmail.com>
2016-02-19 17:10:21 -0800

    Rename coef vars and use equality test

--- test/test.evalrational.js
+++ test/test.evalrational.js
@@ -45,23 +45,23 @@ tape( 'if the value at which to evaluate a rational function is `0`, the functio
 });
 
 tape( 'the function evaluates a rational function', function test( t ) {
-	var num;
-	var denom;
+	var P;
+	var Q;
 	var v;
 
-	num = [ 2, 3, 2 ];
-	denom = [ 1, 0, 0 ];
-	v = evalrational( num, denom, 1 );
+	P = [ 2, 3, 2 ];
+	Q = [ 1, 0, 0 ];
+	v = evalrational( P, Q, 1 );
 	t.equal( v, 7, 'returns 7' );
 
-	num = [ -6, -5 ];
-	denom = [ 3, 0.5 ];
-	v = evalrational( num, denom, 6 );
+	P = [ -6, -5 ];
+	Q = [ 3, 0.5 ];
+	v = evalrational( P, Q, 6 );
 	t.equal( v, -6, 'returns -6' );
 
-	num = [ -19, 7, -4, 6 ];
-	denom = [ 4, 5, 2, 1 ];
-	v = evalrational( num, denom, 3 );
+	P = [ -19, 7, -4, 6 ];
+	Q = [ 4, 5, 2, 1 ];
+	v = evalrational( P, Q, 3 );
 	t.equal( v, 2, 'returns 2' );
 
 	t.end();
--- test/test.factory.js
+++ test/test.factory.js
@@ -9,7 +9,7 @@ var factory = require( './../lib/factory.js' );
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
-	t.ok( typeof factory === 'function', 'main export is a function' );
+	t.equal( typeof factory, 'function', 'main export is a function' );
 	t.end();
 });
 
